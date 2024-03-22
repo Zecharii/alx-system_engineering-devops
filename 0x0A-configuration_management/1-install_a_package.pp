d5932ef0754b473078d66ce60c24a2b7ac651528
Zechariah <111582433+Zecharii@users.noreply.github.com>
2024-03-22 17:42:25 +0100

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-# Installs flask, version 2.1.0 from pip3
+# Installs flask, version 2.1.0
 
 package { 'python3-flask':
-ensure => '2.1.0',
-provide => 'pip3',
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
